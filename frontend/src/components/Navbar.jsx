2286a4434dfc269a0dd07e54fa22eecad7d58e54
SANJOY DAS <106918069+sanjoydasjoy@users.noreply.github.com>
2024-10-21 23:59:08 +0600

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useAuth } from "../context/ContextProvider";
 
 const Navbar = () => {
+    const { user } = useAuth()
     return (
         <nav className="bg-gray-800 p-4 text-white flex justify-between items-center">
             <div className="text-x1 font-bold">
@@ -13,17 +15,31 @@ const Navbar = () => {
                 className="bg-gray-600 px-4 py-2 rounded"
             />
             <div>
-                <span className="mr-4">user name</span>
-                <Link to="/login" className="bg-blue-500 px-4 py-2 rounded mr-4">
-                    Login
-                </Link>
-                <Link to="/signup" className="bg-green-500 px-4 py-2 rounded mr-4">
-                    Signup
-                </Link>
 
-                <button className="bg-red-500 px-4 py-2 rounded">
-                    Logout
-                </button>
+                {!user ? (
+                    <>
+                        <Link to="/login" className="bg-blue-500 px-4 py-2 rounded mr-4">
+                            Login
+                        </Link>
+                        <Link to="/signup" className="bg-green-500 px-4 py-2 rounded mr-4">
+                            Signup
+                        </Link>
+                    </>
+
+                )
+                :
+                (
+                    <>
+                            <span className="mr-4">{user.name}</span>
+                            <button className="bg-red-500 px-4 py-2 rounded">
+                                Logout
+                            </button>
+                    </>
+                )
+
+
+                }
+
             </div>
         </nav>
     )
