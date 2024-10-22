28ec20caad99057066ac02dbb070a20b8c5ccbfc
SANJOY DAS <106918069+sanjoydasjoy@users.noreply.github.com>
2024-10-22 23:21:26 +0600

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,10 @@
 import express from 'express'
 import dotenv from 'dotenv';
 import cors from 'cors'
-import authRouter from './routes/auth.js'
 import connectToMongoDB from './config/db.js'
+
+import authRouter from './routes/auth.js'
+import noteRouter from './routes/note.js'
 
 dotenv.config();
 
@@ -11,7 +13,9 @@ const port = process.env.PORT
 const app = express()
 app.use(cors())
 app.use(express.json())
+
 app.use('/api/auth', authRouter)
+app.use('/api/note', noteRouter)
 
 
 app.listen(port, () => {
