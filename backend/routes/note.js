6f054151b90a1cda61e9f04b8e9db7ef8ec6f6b8
SANJOY DAS <106918069+sanjoydasjoy@users.noreply.github.com>
2024-10-23 02:14:34 +0600

    Update note.js

--- backend/routes/note.js
+++ backend/routes/note.js
@@ -4,10 +4,10 @@ import middleware from '../middleware/middleware.js';
 
 const noteRouter = express.Router()
 
-noteRouter.post('/add', middleware ,async (req, res) => {
+noteRouter.post('/add', middleware, async (req, res) => {
     try {
         const { title, description } = req.body;
-        
+
 
         const newNote = new Note({
             title,
@@ -27,6 +27,15 @@ noteRouter.post('/add', middleware ,async (req, res) => {
 
     }
 
+})
+
+noteRouter.get('/', async (req, res) => {
+    try {
+        const notes = await Note.find()
+        return res.status(200).json({ success: true, notes })
+    } catch (error) {
+        return res.status(500).json({ success: false, message: "cant retrive notes" })
+    }
 })
 
 export default noteRouter;
