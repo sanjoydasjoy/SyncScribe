5983d98b6b752c35045a5acbe39b19263ffcf77a
SANJOY DAS <106918069+sanjoydasjoy@users.noreply.github.com>
2024-10-23 02:41:08 +0600

    Update NoteModel.jsx

--- frontend/src/components/NoteModel.jsx
+++ frontend/src/components/NoteModel.jsx
@@ -1,18 +1,30 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
-const NoteModal = ({ closeModel, addNote }) => {
+
+const NoteModal = ({ closeModel, addNote, currentNote, updateNote }) => {
     const [title, setTitle] = useState("");
     const [description, setDescription] = useState("");
 
+    useEffect(() => {
+        if (currentNote) {
+            setTitle(currentNote.title)
+            setDescription(currentNote.description)
+        }
+    }, [currentNote])
+
     const handleSubmit = async (e) => {
         e.preventDefault()
-        addNote(title, description)
+        if (currentNote) {
+            updateNote(currentNote._id, title, description)
+        } else {
+            addNote(title, description)
+        }
     }
 
     return (
         <div className="fixed inset-0 bg-gray-800 bg-opacity-75 flex justify-center items-center">
             <div className="bg-white p-8 rounded">
-                <h2 className="text-x1 font-bold mb-4">Add New Note</h2>
+                <h2 className="text-x1 font-bold mb-4">{currentNote ? "Edit Note" : "Add New Note"}</h2>
                 <form onSubmit={handleSubmit}>
                     <input
                         type="text"
@@ -32,7 +44,7 @@ const NoteModal = ({ closeModel, addNote }) => {
                         className="bg-blue-500 text-white px-4 py-2 rounded"
                     >
 
-                        Add Note
+                        {currentNote ? "Update Note" : "Add Note"}
                     </button>
                 </form>
                 <button className="mt-4 text-red-500" onClick={closeModel}>Cancel</button>
