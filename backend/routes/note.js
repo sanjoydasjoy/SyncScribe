de4745928e5385683f2ad6955b8971880c39f991
SANJOY DAS <106918069+sanjoydasjoy@users.noreply.github.com>
2024-10-23 01:25:40 +0600

    Update note.js

--- backend/routes/note.js
+++ backend/routes/note.js
@@ -1,27 +1,29 @@
 import express from 'express'
 import Note from '../models/Note.js'
+import middleware from '../middleware/middleware.js';
 
 const noteRouter = express.Router()
 
-noteRouter.post('/add', async (req, res) => {
+noteRouter.post('/add', middleware ,async (req, res) => {
     try {
         const { title, description } = req.body;
         
 
-        const newUser = new Note({
+        const newNote = new Note({
             title,
             description,
-            
+            userId: req.user.id
+
         })
 
-        await newUser.save()
+        await newNote.save()
 
-        return res.status(200).json({ success: true, message: "Accounted Created Successfully" })
+        return res.status(200).json({ success: true, message: "Note Created Successfully" })
 
     } catch (error) {
         console.log(error.message);
 
-        return res.status(500).json({ success: false, message: "error in adding user" })
+        return res.status(500).json({ success: false, message: "error in adding note" })
 
     }
 
