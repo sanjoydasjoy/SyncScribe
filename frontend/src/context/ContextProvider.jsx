220b917b72bcf8fed9f9ffa83dac7c0d12af8fbd
SANJOY DAS <106918069+sanjoydasjoy@users.noreply.github.com>
2024-10-23 21:10:26 +0600

    Update ContextProvider.jsx

--- frontend/src/context/ContextProvider.jsx
+++ frontend/src/context/ContextProvider.jsx
@@ -1,16 +1,36 @@
-import React, { createContext, useContext, useState } from 'react'
+import React, { createContext, useContext, useState, useEffect } from 'react'
 
 const authContext = createContext()
 
-const ContextProvider = ({children}) => {
+const ContextProvider = ({ children }) => {
     const [user, setUser] = useState(null)
     const login = (user) => {
         setUser(user)
     }
-    const logout = ()=>{
+    const logout = () => {
         localStorage.removeItem('token')
         setUser(null)
     }
+    useEffect(() => {
+        const verifyUser = async () => {
+            try {
+                const res = await axios.get('http://localhost:5000/api/auth/verify', {
+                    headers: {
+                        Authorization : `Bearer ${localStorage.getItem("token")}`
+                    }
+                })
+                if (res.data.success) {
+                    setUser(res.data.user)
+                } else[
+                    setUser(null)
+                ]
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        verifyUser()
+    }, [])
+
     return (
         <authContext.Provider value={{ user, login, logout }}>
             {children}
@@ -20,3 +40,4 @@ const ContextProvider = ({children}) => {
 }
 export const useAuth = () => useContext(authContext)
 export default ContextProvider
+
