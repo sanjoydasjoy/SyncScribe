90d77078190588c68d6d2df3edb5206771724940
SANJOY DAS <106918069+sanjoydasjoy@users.noreply.github.com>
2024-10-23 21:10:48 +0600

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,17 +2,22 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import SignUp from "./pages/SignUp"
 import Login from "./pages/Login"
+import {ToastContainer} from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 
 function App() {
 
   return(
 
-    <Routes>
-      <Route path="/" element={<Home />}></Route>
-      <Route path="/signup" element={<SignUp />}></Route>
-      <Route path="/login" element={<Login />}></Route>
-    </Routes>
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+      <ToastContainer />
+    </div>
 
   )
 }
